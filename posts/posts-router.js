6105680bb83c920e8f8022f945d75cb3ae80be06
John Wells <molekewl2017@gmail.com>
2020-03-15 02:55:44 -0700

    get: all user posts & post: new post & post: commment

---
+++ posts/posts-router.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+const db = require("../data/db");
+
+const router = express.Router();
+
+// Creates a post using the info sent inside the request body
+router.post("/", (req, res) => {
+  console.log(req.body);
+  db.insert(req.body)
+    .then(post => {
+      console.log(post);
+      res.status(201).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Error adding post" });
+    });
+});
+
+// Creates a comment for the post with the specified id
+// using info sent inside the request body
+router.post("/:id/comments", (req, res) => {
+  const id = req.params.id;
+  const comment = {...req.body, post_id: id}
+
+  db.findById(id)
+    .then(post => {
+      post.length ?
+        db.insertComment(comment)
+          .then(data => res.status(201).json(comment))
+          .catch(err => {
+            res.status(500).json({
+              error: 'Error saving comment to post'
+            })
+          })
+      : res.status(400).json({ 
+        message: 'The post with the specified ID does not exist'
+        });
+  });
+});
+
+// returns an array of all post objects contained in the database
+router.get("/", (req, res) => {
+  db.find(req.query)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "error retrieving all posts" });
+    });
+});
+
+module.exports = router;
